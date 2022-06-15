eac1537ea21b33a2b313847b20b650fd217bf16f
LuisMM24 <luismateo2416@gmail.com>
2022-06-15 09:12:41 +0200

    location and users following profile

--- src/components/organism/UserPersonalInfo/UserPersonalInfo.tsx
+++ src/components/organism/UserPersonalInfo/UserPersonalInfo.tsx
@@ -6,6 +6,9 @@ import PrimaryButton from '../../atoms/PrimaryButton/PrimaryButton'
 import NickContainer from '../../molecules/NickContainer/NickContainer'
 // styles
 import "./UserPersonalInfo.css"
+// icons
+import usersIcon from "../../../assets/img/users.svg"
+import locationIcon from "../../../assets/img/location.svg"
 const UserPersonalInfo: React.FC = () => {
     const userProfile = useContext(UserProfileContext)
     return (
@@ -17,6 +20,19 @@ const UserPersonalInfo: React.FC = () => {
             />
             <p className='userBio'>{userProfile?.bio}</p>
             <PrimaryButton type='button' className='followUserBtn'>Follow</PrimaryButton>
+            <div className='followersFollowingContainer'>
+                <div className='followersWrapper'>
+                    <img src={usersIcon} alt="users" />
+                    <p>{userProfile?.followers} <span className='grayText'>followers</span></p>
+                </div>
+                ◦ <p>{userProfile?.following} <span className='grayText'>following</span></p>
+            </div>
+            {userProfile?.location && (
+                <div className='locationWrapper'>
+                    <img src={locationIcon} alt="location" /> {userProfile?.location}
+                </div>
+            )}
+
         </>
     )
 }
