95aa9cc4a49dc8297f784120a0b709566b3f0009
LuisMM24 <luismateo2416@gmail.com>
2022-06-17 12:16:07 +0200

    readme.md

--- README.md
+++ README.md
@@ -3,6 +3,14 @@
 This project was done for a technical challenge to MVST.
 It's based in github, basically you can filter the repositories of the current user profile
 
+Project built with:
+
+- React
+- React-testing-library
+- Typescript
+- CSS
+- Storybook
+
 > Atomic design
 
 This project was done using an atomic design, which components folder have specific structure:
@@ -50,6 +58,10 @@ npm run coverage
 ```
 
 ## Future improvements
+
+- Improve the query system with Graphql using the github v4 API
+- You should see the entire user profile, navigate between the overview, repos, projects, packages, etc.
+- Improve the filter system, you can filter by lastest commited projects, by language, or sort by name.
 
 ## Feedback
 
--- src/components/organism/UserRepositories/UserRepositories.test.tsx
+++ src/components/organism/UserRepositories/UserRepositories.test.tsx
@@ -1,5 +1,5 @@
 // testing utilities
-import { screen, render } from "@testing-library/react";
+import { screen, render, waitFor } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect"
 // components
 import UserRepositories from "./UserRepositories";
@@ -8,26 +8,53 @@ import { UserProfileContext } from "../../../context/UserProfileContext";
 // memory router
 import { MemoryRouter } from "react-router-dom";
 
-const mockResponse = {
+const mockResponse = [{
 
     name: "repo test",
     private: false
 
-}
+}]
 
 
-// jest.spyOn(global, 'fetch').mockImplementation(() =>
-//     Promise.resolve({
-//         json: () => Promise.resolve(mockResponse),
-//     } as Response),
-// );
+
 
 describe("UserRepositories component", () => {
-    beforeEach(() => jest.clearAllMocks())
+    jest.spyOn(global, 'fetch').mockImplementation(() =>
+        Promise.resolve({
+            json: () => Promise.resolve(mockResponse),
+        } as Response),
+    );
     it("should render", () => {
         render(<UserRepositories />)
         const inputText = screen.getByPlaceholderText(/find a repository/i)
         expect(inputText).toBeInTheDocument()
+
+    })
+    it("should fetch repos", async () => {
+        const userMock = {
+            avatar_url: "https://avatars.githubusercontent.com/u/91225705?v=4",
+            bio: "Hi, my name is Luis, im 22 years old.\r\nI'm a front end developer and I love playing the guitar and videogames.",
+            followers: 4,
+            following: 8,
+            repos_url: "https://api.github.com/users/LuisMM24/repos",
+            location: "España, Castilla la mancha",
+            login: "LuisMM24",
+            name: "Luis Molina",
+        }
+        render(
+            <MemoryRouter>
+                <UserProfileContext.Provider value={
+                    {
+                        userProfile: userMock,
+                        isLoading: false,
+                        error: null
+                    }
+                }>
+                    <UserRepositories />
+                </UserProfileContext.Provider>
+            </MemoryRouter>
+        )
+        await waitFor(async () => expect(await screen.findByText(/loading/i)).toBeNull())
 
     })
 
